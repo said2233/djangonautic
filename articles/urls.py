4d48f1f5a45acc3ef5aae7220e9ddd56d9745ff1
Said Said Hussein <77249581+said2233@users.noreply.github.com>
2023-09-30 18:16:15 +0300

    i made changes

--- articles/urls.py
+++ articles/urls.py
@@ -1,10 +1,10 @@
 
-from django.contrib import admin
+# from django.contrib import admin
 from django.urls import path
 from.import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('about/',views.about),
-    path('',views.homepage),
+    # path('admin/', admin.site.urls),
+    # path('about/',views.about),
+    path('',views.article_list),
 ]
